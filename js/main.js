742cd9605b504f9e9e9194ba803fc6b429f95759
AleksF2022 <118790177+AleksF2022@users.noreply.github.com>
2023-05-08 21:26:09 +0300

    Add files via upload
    First commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// const apiKey = 'e1925445427c432483395911230805'
+// const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Vyzhnytsya`;
+// fetch(query).then((responce) => {
+//   return responce.json()
+// }).then((data) => {
+//   console.log(data);
+// })
