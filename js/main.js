75218d3fb44e4a8c9e19e5a21691ecd49ded1b22
AleksF2022 <118790177+AleksF2022@users.noreply.github.com>
2023-05-09 02:09:03 +0300

    Add files via upload
    Second commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,52 @@
-// const apiKey = 'e1925445427c432483395911230805'
-// const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Vyzhnytsya`;
-// fetch(query).then((responce) => {
-//   return responce.json()
-// }).then((data) => {
-//   console.log(data);
-// })
+// Элементы на странице
+const header = document.querySelector(".header");
+const form = document.querySelector("#form");
+const input = document.querySelector("#inputCity");
+
+// Слушаем отправку формы
+form.onsubmit = (e) => {
+  // Отменяем отправку формы
+  e.preventDefault();
+  //  Берем значение из инпута, обрезаем пробелы
+  let city = input.value.trim();
+  // Адрес запроса
+  const apiKey = "e1925445427c432483395911230805";
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  //  Выполняем запрос
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // Проверка на ошибку
+      if (data.error) {
+        // Если есть ошибка
+        //  Удаляем предыдущую карточку
+        const prevCard = document.querySelector(".card");
+        if (prevCard) prevCard.remove();
+
+        // Отображаем карточку с ошибкой
+        const html = `<div class="card">${data.error.message}</div>`;
+
+        // Отображаем карточку на странице
+        header.insertAdjacentHTML("afterend", html);
+      } else {
+        //  Ошибки нет- выводим карточку
+        //  Отображаем полученные данные в карточке
+        //  Удаляем предыдущую карточку
+        const prevCard = document.querySelector(".card");
+        if (prevCard) prevCard.remove();
+        // Разметка для карточки
+        const html = `<div class="card">
+        <h2 class="card-city">${data.location.name}<span>${data.location.country}</span></h2>
+        <div class="card-weather">
+        <div class="card-value">${data.current.temp_c}<sup>°c</sup></div>
+        <img class="card-img" src="images/example.png" alt="Weather">
+        </div>
+        <div class="card-description">${data.current.condition.text}</div>
+        </div>`;
+        // Отображаем карточку на странице
+        header.insertAdjacentHTML("afterend", html);
+      }
+    });
+};
